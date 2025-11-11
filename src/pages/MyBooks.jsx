fda86a559d7d0ef71fdc238b45e58eea347c8dd8
sadnur Islam <sadnurislam2000@gmail.com>
2025-11-11 13:47:50 +0600

    create my books page

--- src/pages/MyBooks.jsx
+++ src/pages/MyBooks.jsx
@@ -1,9 +1,84 @@
 import React from 'react';
+import { Star } from 'lucide-react';
+import { FaPlus } from 'react-icons/fa';
+import { Link } from 'react-router';
 
 const MyBooks = () => {
+    const books = [
+        { _id: 1, title: 'Book One', author: 'Author A', rating: 4.5 },
+        { _id: 2, title: 'Book Two', author: 'Author B', rating: 4.0 },
+        { _id: 3, title: 'Book Three', author: 'Author C', rating: 3.5 },
+    ]
     return (
-        <div>
-            
+        <div className='my-10'>
+            <h2 className='text-4xl font-bold mx-auto mb-3'>My Books</h2>
+            <h4 className='opacity-60 mx-auto'>You have {books.length} books in your collection.</h4>
+            <div className='text-end mb-4'>
+                <Link to='/add-book' className="btn btn-primary px-5 rounded-lg font-bold mt-2">
+                    <span><FaPlus /></span> <span>Add Book</span>
+                </Link>
+            </div>
+            <div className="p">
+                <div className="overflow-x-auto rounded-lg shadow-md">
+                    <table className="table table-zebra w-full">
+                        <thead className="bg-base-200">
+                            <tr className="text-lg font-semibold row-border">
+                                <th>Title</th>
+                                <th>Author</th>
+                                <th>Genre</th>
+                                <th>Rating</th>
+                                <th className='text-center'>Actions</th>
+                            </tr>
+                        </thead>
+                        <tbody className="">
+
+
+                            {books?.map((book) => (
+                                <tr key={book._id} className="hover odd:bg-base-100 even:bg-base-200 row-border">
+                                    <td className="font-medium">{book.title}</td>
+                                    <td>{book.author}</td>
+                                    <td>Fiction</td>
+                                    <td>
+                                        <div className="flex items-center gap-1 text-yellow-400">
+                                            <Star key={0} size={16} fill="currentColor" />
+                                            {
+                                                book.rating > 1 ?
+                                                    <Star key={1} size={16} fill="currentColor" /> :
+                                                    <Star key={1} size={16} />
+
+                                            }
+                                            {
+                                                book.rating > 2 ?
+                                                    <Star key={2} size={16} fill="currentColor" /> :
+                                                    <Star key={2} size={16} />
+
+                                            }
+                                            {
+                                                book.rating > 3 ?
+                                                    <Star key={3} size={16} fill="currentColor" /> :
+                                                    <Star key={3} size={16} />
+
+                                            }
+                                            {
+                                                book.rating > 4 ?
+                                                    <Star key={4} size={16} fill="currentColor" /> :
+                                                    <Star key={4} size={16} />
+                                            }
+
+                                        </div>
+                                    </td>
+
+                                    <td className="flex gap-2 justify-center">
+                                        <button className=" btn btn-primary btn-sm rounded-lg">View</button>
+                                        <button className=" btn  btn-sm rounded-lg btn-warning text-white">Update</button>
+                                        <button className=" btn btn-sm rounded-lg btn-error text-white  ">Delete</button>
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            </div>
         </div>
     );
 };

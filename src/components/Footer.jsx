fea367685e895774dded3b2114bd573d62854374
sadnur Islam <sadnurislam2000@gmail.com>
2025-11-11 22:08:40 +0600

    update footer ui

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,50 @@
 import React from 'react';
+import { FaXTwitter } from 'react-icons/fa6';
+import { MdLocalLibrary } from 'react-icons/md';
 
 const Footer = () => {
     return (
         <div>
-            <div className="my-footer py-7 text-center">
-                <div>
-                    <p>Copyright © {new Date().getFullYear()} - All right reserved by The Book Heaven</p>
-                </div>
-            </div>
+            <footer className="footer footer-horizontal footer-center my-footer py-12 text-center">
+                <aside>
+                    <MdLocalLibrary size={50} className='logo-color'></MdLocalLibrary>
+                    <p className="">
+                        <span className='text-xl font-bold'>The Book Heaven</span>   
+                        <br />
+                        <span className='font-semibold opacity-80'>Your gateway to endless stories and knowledge.</span>
+                    </p>
+                    <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
+                </aside>
+                <nav>
+                    <div className="grid grid-flow-col gap-4 items-center text-2xl">
+                        <a>
+                            <FaXTwitter />
+                        </a>
+                        <a>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                className="fill-current">
+                                <path
+                                    d="M19.615 3.184c-3.604-.246-11.631-.245-15.23 0-3.897.266-4.356 2.62-4.385 8.816.029 6.185.484 8.549 4.385 8.816 3.6.245 11.626.246 15.23 0 3.897-.266 4.356-2.62 4.385-8.816-.029-6.185-.484-8.549-4.385-8.816zm-10.615 12.816v-8l8 3.993-8 4.007z"></path>
+                            </svg>
+                        </a>
+                        <a>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                className="fill-current">
+                                <path
+                                    d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path>
+                            </svg>
+                        </a>
+                    </div>
+                </nav>
+            </footer>
         </div>
     );
 };
